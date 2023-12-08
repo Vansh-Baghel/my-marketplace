eab7ed12ebe6533789657d9aea95dac4cb6d5263
Light | KONMA <122342177+konmavedant@users.noreply.github.com>
2023-12-08 13:59:09 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,8 @@ import { MeshProvider } from "@meshsdk/react";
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { ReactNode } from 'react';
 
-type AppPropsWithChildren = AppProps & {
-  Component: React.ComponentType<AppProps['pageProps']>;
-  children: ReactNode;
-};
-
-function App({ Component, pageProps }: AppPropsWithChildren) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <MeshProvider>
       <Head>
@@ -21,13 +15,8 @@ function App({ Component, pageProps }: AppPropsWithChildren) {
         <link rel="icon" href="https://meshjs.dev/favicon/favicon-32x32.png" />
       </Head>
       <Navbar />
-      {/* Ensure that Component is a valid JSX element */}
-      {React.isValidElement(Component) ? (
-        <Component {...pageProps} />
-      ) : null}
+      <Component {...pageProps} />
       <Footer />
     </MeshProvider>
   );
 }
-
-export default App;
