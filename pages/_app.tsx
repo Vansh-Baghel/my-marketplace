a6d14d84d1204647b20b5f596409f03bd06257b7
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-12-10 12:17:38 +0530

    Changing to any

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: any) {
   return (
     <MeshProvider>
       <Head>
