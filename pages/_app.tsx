4e0ca63055020477f148c9aad7fc9359bdbb03eb
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-12-10 11:43:03 +0530

    Fixing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
 import { MeshProvider } from "@meshsdk/react";
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
