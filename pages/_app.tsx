c262ce1261ffafd1b4cb0b7ca1a0be07b9f6bb4a
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-12-10 11:56:22 +0530

    Importing correctly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { AppProps } from "next/app";
 import { MeshProvider } from "@meshsdk/react";
 import Head from "next/head";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
